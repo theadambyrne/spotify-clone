5ba2780ebea35e03f95f007d6a69736a70030d12
Adam Byrne <adam.r.byrne@gmail.com>
2020-08-04 00:21:40 +0100

    Updates

---
+++ precache-manifest.f6b2b93fe2bb2b911db1f8e75160bdd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662d00e34888f16bc10e169552fa9ec5",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "6f490cee80d3e7693d06",
+    "url": "/spotify-clone/static/css/main.b1210b00.chunk.css"
+  },
+  {
+    "revision": "2c498fa34b75e86ebf3b",
+    "url": "/spotify-clone/static/js/2.89c26f73.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.89c26f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f490cee80d3e7693d06",
+    "url": "/spotify-clone/static/js/main.322de275.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
